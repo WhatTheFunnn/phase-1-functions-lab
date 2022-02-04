3412f0cc0d5a455155480b7295c87f1a9873e85c
Dan Souza <Souza.music@outlook.com>
2022-02-04 01:34:46 -0800

    stuck with an impossible solution

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+const hq = 42
+let distance = (43, 50, 34)
+const feet = 264
+let travel = (48, 60, 28)
+
+function distanceFromHqInBlocks (distance){
+    if (distance > hq)
+        return distance - hq 
+    else(distance < hq) 
+        return hq - distance 
+}
+
+function distanceFromHqInFeet (feet) {
+    return distanceFromHqInBlocks(feet) * 264
+}
+
+function distanceTravelledInFeet(travel, distance) {
+    let disttrav
+    if (travel > distance){
+        disttrav = travel - distance;
+        return (disttrav * 264)
+}
+    {if (travel < distance)
+        disttrav = distance - travel;
+        return (disttrav * 264)
+    }
+}
+
+function calculatesFarePrice (start, destination) {
+    let ride = distanceFromHqInFeet(start, destination)
+    let price
+    if (ride <= 400){ 
+        price = 0;
+    } else if (ride > 400 && ride <= 2000) {
+        price = (ride - 400) *.02;
+    } else if (ride > 2000 && ride <= 2500) {
+        price = 25;
+    } else {
+        price = 'cannot travel that far';
+    }
+    return price 
+}
